7d7a570ee9fd6255ae0ea05df95be0fbd59e15b1
Noah Hendrix <noahhendrix@gmail.com>
2011-12-10 02:44:13 -0600

    launch layout

---
+++ js/functions.js
@@ -0,0 +1,28 @@
+// remap jQuery to $
+(function($){})(window.jQuery);
+
+
+/* trigger when page is ready */
+$(document).ready(function (){
+
+$('#arm').plaxify({"xRange":16,"yRange":16,"invert":true})
+$('#body').plaxify({"xRange":5,"yRange":5,"invert":true})
+$('#head').plaxify({"xRange":8,"yRange":2})
+$('#badge').plaxify({"xRange":5,"yRange":5,"invert":true})
+$.plax.enable()
+
+
+});
+
+
+/* optional triggers
+
+$(window).load(function() {
+	
+});
+
+$(window).resize(function() {
+	
+});
+
+*/
